6a7fc4405e4ed358319327acccb05bc168c101c3
Matt Hoover <matthew.a.hoover@gmail.com>
2017-11-04 12:54:21 -0400

    Update API class to return data directly.

--- __init__.py
+++ __init__.py
@@ -4,38 +4,67 @@ import requests
 class Cap1API(object):
     EP = 'https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-hackathon'
 
+    @staticmethod
+    def assert200(call):
+        assert call.status_code==200
+
     def accounts(self, acctid=None):
         if acctid:
             body = {
                 'account_id': acctid,
             }
-            return requests.post('{}/accounts'.format(self.EP), json=body)
+            res = requests.post('{}/accounts'.format(self.EP), json=body)
         else:
-            return requests.post('{}/accounts'.format(self.EP))
+            res = requests.post('{}/accounts'.format(self.EP))
+        try:
+            self.assert200(res)
+            return res.json()
+        except AssertionError:
+            print('Bad call; {}'.format(res.text))
 
     def customers(self, custid=None):
         if custid:
             body = {
                 'customer_id': custid,
             }
-            return requests.post('{}/customers'.format(self.EP), json=body)
+            res = requests.post('{}/customers'.format(self.EP), json=body)
         else:
-            return requests.post('{}/customers'.format(self.EP))
+            res = requests.post('{}/customers'.format(self.EP))
+        try:
+            self.assert200(res)
+            return res.json()
+        except AssertionError:
+            print('Bad call; {}'.format(res.text))
 
     def transactions(self, acctid):
         body = {
             'account_id': acctid,
         }
-        return requests.post('{}/transactions'.format(self.EP), json=body)
+        res = requests.post('{}/transactions'.format(self.EP), json=body)
+        try:
+            self.assert200(res)
+            return res.json()
+        except AssertionError:
+            print('Bad call; {}'.format(res.text))
 
     def rewards(self, acctid):
         body = {
             'account_id': acctid,
         }
-        return requests.post('{}/rewards'.format(self.EP), json=body)
+        res = requests.post('{}/rewards'.format(self.EP), json=body)
+        try:
+            self.assert200(res)
+            return res.json()
+        except AssertionError:
+            print('Bad call; {}'.format(res.text))
 
     def payments(self, acctid):
         body = {
             'account_id': acctid,
         }
-        return requests.post('{}/payments'.format(self.EP), json=body)
+        res = requests.post('{}/payments'.format(self.EP), json=body)
+        try:
+            self.assert200(res)
+            return res.json()
+        except AssertionError:
+            print('Bad call; {}'.format(res.text))
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -7,44 +7,44 @@ from cap1Hackathon import *
 
 def test_api_accounts():
     result = Cap1API().accounts()
-    assert isinstance(result.json(), list)
+    assert isinstance(result, list)
 
 
 def test_api_accounts_record():
     result = Cap1API().accounts(100300000)
-    assert isinstance(result.json(), list)
-    assert len(result.json())==1
-    assert isinstance(result.json()[0], dict)
+    assert isinstance(result, list)
+    assert len(result)==1
+    assert isinstance(result[0], dict)
 
 
 def test_api_customers():
     result = Cap1API().customers()
-    assert isinstance(result.json(), list)
+    assert isinstance(result, list)
 
 
 def test_api_customers_record():
     result = Cap1API().customers(101710000)
-    assert isinstance(result.json(), list)
-    assert len(result.json())==1
-    assert isinstance(result.json()[0], dict)
+    assert isinstance(result, list)
+    assert len(result)==1
+    assert isinstance(result[0], dict)
 
 
 def test_api_transactions():
     result = Cap1API().transactions(100300000)
-    assert isinstance(result.json(), list)
-    assert len(result.json())>=1
-    assert isinstance(result.json()[0], dict)
+    assert isinstance(result, list)
+    assert len(result)>=1
+    assert isinstance(result[0], dict)
 
 
 def test_api_rewards():
     result = Cap1API().rewards(100300000)
-    assert isinstance(result.json(), list)
-    assert len(result.json())>=1
-    assert isinstance(result.json()[0], dict)
+    assert isinstance(result, list)
+    assert len(result)>=1
+    assert isinstance(result[0], dict)
 
 
 def test_api_payments():
     result = Cap1API().payments(100300000)
-    assert isinstance(result.json(), list)
-    assert len(result.json())>=1
-    assert isinstance(result.json()[0], dict)
+    assert isinstance(result, list)
+    assert len(result)>=1
+    assert isinstance(result[0], dict)
